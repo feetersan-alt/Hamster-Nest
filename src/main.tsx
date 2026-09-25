1058f3fc8aa0c188b9af1b17f70c941cd0f92518
feetersan-alt <feetersan@gmail.com>
2026-09-25 20:05:06 +0800

    Route OAuth consent outside HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import './index.css'
 import './styles/ui.css'
 import App from './App.tsx'
+import ConsentPage from './pages/ConsentPage'
 import { initializeAppServiceWorker } from './lib/serviceWorker'
 
 const noFxEnabled =
@@ -14,11 +15,11 @@ if (noFxEnabled) {
   document.documentElement.classList.add('no-fx')
 }
 
+const isOAuthConsentPath = window.location.pathname.endsWith('/oauth/consent')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    {isOAuthConsentPath ? <ConsentPage /> : <HashRouter><App /></HashRouter>}
   </StrictMode>,
 )
 
